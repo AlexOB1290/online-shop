bcbf864a8aefe5836dfd4f404c51845afb3a7284
AlexOB1290 <rougetrooper2016@gmail.com>
2024-11-13 21:55:42 +0800

    производит небольшие изменения в handle-registration

--- src/public/handle_registration.php
+++ src/public/handle_registration.php
@@ -5,12 +5,12 @@ $email = $_GET['email'];
 $password = $_GET['psw'];
 $passwordRepeat = $_GET['psw-repeat'];
 
-print_r("{$name} - {$email} - {$password} - {$passwordRepeat}");
+//print_r("{$name} - {$email} - {$password} - {$passwordRepeat}");
 
 $pdo = new PDO('pgsql:host=postgres;port=5432;dbname=mydb', 'user', 'pass');
 
 $pdo->exec("INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')");
 
-$result = $pdo->query("SELECT * FROM users ORDER BY id DESC");
+$result = $pdo->query("SELECT * FROM users ORDER BY id DESC LIMIT 1");
 
 print_r($result->fetch());
